3ad4595712b11c8be95175f7ec76873b925a5a65
JA9901 <jareyes999@gmail.com>
2022-10-04 22:31:09 -0400

    updated

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+import Todo from "./components/Todo.jsx";
+import Title from "./components/Title.jsx";
+import Modal from "./components/Modal.jsx";
+
+function App() {
+  return (
+    <div>
+      <Title />
+      <div>
+        <input type="text" onChange={(event) => {
+          console.log(event.target.value)
+        }} />
+        <button>Add todo</button>
+      </div>
+      <div className="todo__wrapper">
+        <Todo title="Finish Fronend Simplified" />
+        <Todo title="Finish Interview Section" />
+        <Todo title="Land a $100k job" />
+      </div>
+      <Modal title="Are you sure you want to delete?"/>
+    </div>
+  );
+}
+
+export default App;
