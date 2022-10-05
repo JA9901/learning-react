24fe643d06e6172be6da6dd9ea566fb73a5c70e7
JA9901 <jareyes999@gmail.com>
2022-10-05 04:40:07 -0400

    learning more on react

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,53 @@ import "./App.css";
 import Todo from "./components/Todo.jsx";
 import Title from "./components/Title.jsx";
 import Modal from "./components/Modal.jsx";
+import Counter from "./components/Counter.jsx";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+
+  function onTodoDelete() {
+    setShowModal(true);
+    console.log("onTodoDelete()");
+  }
+
+  function cancelModal() {
+    setShowModal(false);
+  }
+
+  function confirmModal() {
+    setShowModal(false);
+  }
+
+  useEffect(() => {
+    console.log('on mount')
+  }, [showModal])
+
   return (
     <div>
       <Title />
       <div>
-        <input type="text" onChange={(event) => {
-          console.log(event.target.value)
-        }} />
-        <button>Add todo</button>
+        <input
+          type="text"
+          onChange={(event) => {
+            console.log(event.target.value);
+          }}
+        />
+        <button onClick={() => setShowModal(true)}>Add todo</button>
       </div>
       <div className="todo__wrapper">
-        <Todo title="Finish Fronend Simplified" />
-        <Todo title="Finish Interview Section" />
-        <Todo title="Land a $100k job" />
+        <Todo onTodoDelete={onTodoDelete} title="Finish Fronend Simplified" />
+        <Todo onTodoDelete={onTodoDelete} title="Finish Interview Section" />
+        <Todo onTodoDelete={onTodoDelete} title="Land a $100k job" />
       </div>
-      <Modal title="Are you sure you want to delete?"/>
+      {showModal && (
+        <Modal
+          cancelModal={cancelModal}
+          confirmModal={confirmModal}
+          title="Confirm delete?"
+        />
+      )}
     </div>
   );
 }
